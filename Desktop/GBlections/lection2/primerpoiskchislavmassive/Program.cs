7d2f40c36d226ccf24908be96c4fc8bf66092f7a
Seredkinsv <ss20003@ya.ru>
2023-03-26 12:45:07 +0300

    init

---
+++ Desktop/GBlections/lection2/primerpoiskchislavmassive/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поиск заданного числа в массиве
+int[] array = { 11, 21, 31, 41, 1115, 61, 127, 18, 19 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index+1;
+    index++;
+}
