d8a53023c54e1ed80a42be41f7d24ecdd4eea6d3
Seredkinsv <ss20003@ya.ru>
2023-03-26 12:48:08 +0300

    init1

--- Desktop/GBlections/lection2/primerpoiskchislavmassive/Program.cs
+++ Desktop/GBlections/lection2/primerpoiskchislavmassive/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Поиск заданного числа в массиве
-int[] array = { 11, 21, 31, 41, 1115, 61, 127, 18, 19 };
+int[] array = { 11, 21, 31, 41, 1115, 61, 127, 11, 18, 19 };
 
 int n = array.Length;
-int find = 18;
+int find = 11;
 
 int index = 0;
 while (index < n)
@@ -10,6 +10,8 @@ while (index < n)
     if(array[index] == find)
     {
         Console.WriteLine(index);
+        // вводим break чтобы наядя первое число больше не искал
+        break;
     }
     //index = index+1;
     index++;
