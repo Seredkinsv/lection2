c9f71016acab0f0708ff550003c1656a27c5d147
Seredkinsv <ss20003@ya.ru>
2023-03-26 12:33:59 +0300

    init

---
+++ Desktop/GBlections/lection2/primermassiv/Program.cs
@@ -0,0 +1,18 @@
+﻿// Поиск максимального числа из введенных девяти цифр
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+// array index  0   1   2   3   4   5   6     7   8
+int[] array = { 11, 21, 31, 41, 1115, 61, 127, 18, 19 };
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+    
+    );
+    Console.WriteLine(max);
